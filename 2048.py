3bdbdc2c58a5b13b53eb0405dcea7f5cdc4b04bf
Omari_Mourning <Ojmourning06@gmail.com>
2024-02-03 15:10:23 -0600

    Update 2048.py

--- 2048.py
+++ 2048.py
@@ -4,8 +4,33 @@ WIDTH, HEIGHT = 600,650
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 color = (255,255,255)
 screen.fill(color)
+pygame.draw.rect(screen, (100,100,100), [50,50,500,500])
+pygame.draw.rect(screen, (200,200,200), [70,70,110,110])
+pygame.draw.rect(screen, (200,200,200), [185,70,110,110])
+pygame.draw.rect(screen, (200,200,200), [300,70,110,110])
+pygame.draw.rect(screen, (200,200,200), [415,70,110,110])
+
+pygame.draw.rect(screen, (200,200,200), [70,185,110,110])
+pygame.draw.rect(screen, (200,200,200), [185,185,110,110])
+pygame.draw.rect(screen, (200,200,200), [300,185,110,110])
+pygame.draw.rect(screen, (200,200,200), [415,185,110,110])
+
+pygame.draw.rect(screen, (200,200,200), [70,300,110,110])
+pygame.draw.rect(screen, (200,200,200), [185,300,110,110])
+pygame.draw.rect(screen, (200,200,200), [300,300,110,110])
+pygame.draw.rect(screen, (200,200,200), [415,300,110,110])
+
+pygame.draw.rect(screen, (200,200,200), [70,415,110,110])
+pygame.draw.rect(screen, (200,200,200), [185,415,110,110])
+pygame.draw.rect(screen, (200,200,200), [300,415,110,110])
+pygame.draw.rect(screen, (200,200,200), [415,415,110,110])
+
+pygame.font.init()
+font = pygame.font.Font('freesansbold.ttf', 32)
+text = font.render("1", True, (0,0,0))
+textRect = text.get_rect()
+
 pygame.display.flip()
-# pygame.draw.rect(screen, (200,200,200), [])
 
 run = True
 while run:
